13ba9c16e21b22ca4c0f577635b07698757312fe
fahimmulla <37366000+fahimmulla@users.noreply.github.com>
2018-04-10 15:10:04 +0100

    new commit

--- calscript.js
+++ calscript.js
@@ -1,8 +1,14 @@
 
 function add() {
-    var n1 = parseFloat(document.getElementById('n1').value);
-    var n2 = parseFloat(document.getElementById('n2').value);
+    var value1 = parseFloat(document.getElementById('n1').value);
+    var value2 = parseFloat(document.getElementById('n2').value);
     var result = n1 + n2;
     alert(result);
 
+}
+
+function square() {
+    var value1 = parseFloat(document.getElementById('n3').value);
+    var results = n3 * n3;
+    alert(results);
 }
